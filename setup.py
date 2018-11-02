04971781916883381b4a2e6d2f9f143d3004e395
Andrea Mostosi <andrea.mostosi@zenkay.net>
2018-11-02 11:14:47 +0100

    Template init

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="target-kinesis",
+    version="0.1.0",
+    description="Singer.io target for extracting data",
+    author="Stitch",
+    url="http://singer.io",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["target_kinesis"],
+    install_requires=[
+        "singer-python>=5.0.12",
+    ],
+    entry_points="""
+    [console_scripts]
+    target-kinesis=target_kinesis:main
+    """,
+    packages=["target_kinesis"],
+    package_data = {},
+    include_package_data=True,
+)
